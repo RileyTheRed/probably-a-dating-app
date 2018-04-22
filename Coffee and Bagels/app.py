3d6312990db9c6e30384228215a8eed3904d04d9
Denis Laesker <dlaesker@gmail.com>
2018-04-22 13:38:58 -0400

    templating done!

--- Coffee and Bagels/app.py
+++ Coffee and Bagels/app.py
@@ -26,5 +26,9 @@ def login():
 def signup():
     return render_template('signup.html', questions=questions)
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
