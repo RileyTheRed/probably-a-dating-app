e96dcb9417c7d1c3e58a8288e3ac7ea77357332c
Riley W <r.wells6894@gmail.com>
2018-09-04 14:33:13 -0400

    Reg/Login Functionality, Final Look-and-feel, removed bloat folders
    Removed bloat files and folders
    Fixed all registration and login functionality
    Finalized look and feel for the About page
    
    Overall, this is the working final version of website.

--- Coffee and Bagels/app.py
+++ Coffee and Bagels/app.py
@@ -1,144 +1,8 @@
-<<<<<<< HEAD
-import os
 import sqlite3 as sql
-from flask import Flask, request, render_template, flash
-from flask_wtf import FlaskForm
-from wtforms import BooleanField, PasswordField, SubmitField, TextField, SelectField, RadioField
-from flask_mail import Mail, Message
-from wtforms.validators import InputRequired, DataRequired, Email, EqualTo, Length
-from wtforms import ValidationError
-
-questions = [{"1":"When I make a plan, I stick to it."},{"2":"I take time out of my day for others."},
-    {"3":"I feel unable to deal with things."},{"4":"I love to help others."},{"5":"I seek adventure."},
-    {"6":"The first place I put my dirty clothes is in the hamper."},{"7":"I often carry the conversation to a higher level."},
-    {"8":"I am a morning peson."},{"9":"I other make others feel good."},{"10":"I am good at analyzing problems."},
-    {"11":"I usually stand up for myself."},{"12":"I am easily discouraged."},{"13":"I can handle a lot of information."},
-    {"14":"Communication is very important in a relationship."},
-    {"15":"Being honest about a tough issue is better than lying about it to avoid the topic."},
-    {"16":"Reliability is an honorable trait in a person."},{"17":"I am patient about things out of my control."},
-    {"18":"Sexual chemistry is very important in a relationship."},{"19":"If you are in a relationship it is still okay to flirt with other people."},
-    {"20":"I believe in true love."}]
-
-email = "test@gmail.com"
-
-app = Flask(__name__)
-
-
-app.config['SECRET_KEY'] = 'SOMETHINGGOESHERE'
-
-#Flask-Mail:
-#Configuring the email server and authentication for the contact page
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_DEBUG'] = True
-app.config['MAIL_USERNAME'] = 'developmentConfig@gmail.com'
-app.config['MAIL_PASSWORD'] = '@config123456'
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-
-mail = Mail(app)
-
-sender = 'developmentConfig@gmail.com'
-
- 
-def send_email(subject, sender, recipients, text_body):
-    with app.app_context():
-        msg = Message(subject, sender=sender, recipients=[recipients])
-        msg.body = text_body
-        mail.send(msg)
-
-#send_email('hello', sender, ['dlaesker@mail.usf.edu'], 'hello')
-        
-class LoginForm(FlaskForm):
-    user_email = TextField('Email Address', validators=[InputRequired(), Length(min=4, max=15)], render_kw={"placeholder": "Email Address"})
-    user_pass = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=80)], render_kw={"placeholder": "Password"})
-
-class SignupForm(FlaskForm):
-    first_name = TextField('First Name', validators=[InputRequired(message='Enter a First Name!'), Length(min=4, max=15)], render_kw={"placeholder": "First Name"})
-    last_name = TextField('Last Name', validators=[InputRequired(message='Enter a Last Name!'), Length(min=4, max=15)], render_kw={"placeholder": "Last Name"})
-    user_email = TextField('Email', validators=[InputRequired(message="You must enter an email!"), Email(message='Invalid email'), Length(max=50)], render_kw={"placeholder": "Email"})
-    user_pass = PasswordField('Password', validators=[InputRequired(message='You must enter a password!'), Length(min=4, max=15)], render_kw={"placeholder": "Password"})
-    user_gender = SelectField('What is your gender?', choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
-    user_want_gender = SelectField('Who are you looking to be matched with?', choices=[ ('female', 'Female'), ('male', 'Male'), ('no_preference', 'No preference')])
-    take_coffee = SelectField('How do you take your coffee?', choices=[('black', 'Black'), ('wcream', 'With Cream'), ('wmilk', 'With Milk')])
-
-class Questionnaire(FlaskForm):
-    L = [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')]
-    q_one = RadioField(questions[0]['1'], choices=L)
-    q_two = RadioField(questions[1]['2'], choices=L)
-    q_three = RadioField(questions[2]['3'], choices=L)
-    q_four = RadioField(questions[3]['4'], choices=L)
-    q_five = RadioField(questions[4]['5'], choices=L)
-    q_six = RadioField(questions[5]['6'], choices=L)
-    q_seven = RadioField(questions[6]['7'], choices=L)
-    q_eight = RadioField(questions[7]['8'], choices=L)
-    q_nine = RadioField(questions[8]['9'], choices=L)
-    q_ten = RadioField(questions[9]['10'], choices=L)
-    
-class ContactForm(FlaskForm):
-    name = TextField('Name', validators=[InputRequired(message='Name required!')],              render_kw={"placeholder": "Enter name"})
-    email = TextField('Email', validators=[InputRequired(message="You must enter an             email!"), Email(message='Invalid email'), Length(max=50)], render_kw=               {"placeholder": "Enter email"})
-    subject = SelectField('Subject', validators=[InputRequired(message="error")], choices=[('Coffee and Bagels', 'General Customer Service'),                 ('Coffee and Bagels', 'Suggestions')])
-    message = TextField('Message', validators=[InputRequired(message=''), Length(min=4,           max=15)], render_kw={"placeholder": "Enter your message here"})
-    
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    #if login information in database -> return 'dashboard'
-    return render_template('login.html', form=form)
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-    contactform = ContactForm()
-    if contactform.validate_on_submit():
-         send_email(contactform.subject.data, sender, contactform.email.data, contactform.message.data)
-    return render_template('contact.html', contactform=contactform)
-
-#################
-#LOOK HERE RILEY#
-#################
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = SignupForm()
-    Q = Questionnaire()
-    #You can access the data by doing 'form.<member variable>.data'
-    #member variables: look in the SignupForm class
-    
-    if form.validate_on_submit():
-        return form.first_name.data
-    #if user not yet in database -> register
-    
-    #else -> print error message
-    return render_template('signup.html', questions=questions, form=form, Q=Q)
-
-# @app.route('/signup', methods=['GET', 'POST'])
-# def signup():
-#     form = SignupForm(request.form)
-#     if request.method == 'POST' and form.validate():
-#         # user = User(form.username.data, form.email.data,
-#         #             form.password.data)
-#         # db_session.add(user)
-#         flash('Thanks for registering')
-#         return redirect(url_for('login'))
-#     return render_template('signup.html', form=form)
-
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html', user_email=email)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-=======
-import sqlite3 as sql
-from flask import Flask, request, render_template, flash
+import functions as f
+import match as m
+import liked as l
+from flask import Flask, request, render_template, flash, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, SubmitField, TextField, SelectField, RadioField
 from flask_mail import Mail, Message
@@ -185,16 +49,16 @@ def send_email(subject, sender, recipients, text_body):
 #send_email('hello', sender, ['dlaesker@mail.usf.edu'], 'hello')
         
 class LoginForm(FlaskForm):
-    user_email = TextField('Email Address', validators=[InputRequired(), Length(min=4, max=15)], render_kw={"placeholder": "Email Address"})
-    user_pass = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=80)], render_kw={"placeholder": "Password"})
+    user_email = TextField('Email Address', validators=[InputRequired(message="Email required!"), Length(min=4, max=40)], render_kw={"placeholder": "Email Address"})
+    user_pass = PasswordField('Password', validators=[InputRequired(message="Password required!"), Length(min=4, max=80)], render_kw={"placeholder": "Password"})
 
 class SignupForm(FlaskForm):
     first_name = TextField('First Name', validators=[InputRequired(message='Enter a First Name!'), Length(min=4, max=15)], render_kw={"placeholder": "First Name"})
     last_name = TextField('Last Name', validators=[InputRequired(message='Enter a Last Name!'), Length(min=4, max=15)], render_kw={"placeholder": "Last Name"})
     user_email = TextField('Email', validators=[InputRequired(message="You must enter an email!"), Email(message='Invalid email'), Length(max=50)], render_kw={"placeholder": "Email"})
     user_pass = PasswordField('Password', validators=[InputRequired(message='You must enter a password!'), Length(min=4, max=15)], render_kw={"placeholder": "Password"})
-    user_gender = SelectField('What is your gender?', choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
-    user_want_gender = SelectField('Who are you looking to be matched with?', choices=[ ('female', 'Female'), ('male', 'Male'), ('no_preference', 'No preference')])
+    user_gender = SelectField('What is your gender?', choices=[('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')])
+    user_want_gender = SelectField('Who are you looking to be matched with?', choices=[ ('Female', 'Female'), ('Male', 'Male'), ('no_preference', 'No preference')])
     take_coffee = SelectField('How do you take your coffee?', choices=[('black', 'Black'), ('wcream', 'With Cream'), ('wmilk', 'With Milk')])
 
 class Questionnaire(FlaskForm):
@@ -210,6 +74,16 @@ class Questionnaire(FlaskForm):
     q_eight = RadioField(questions[7]['8'], choices=L, validators=[InputRequired(message=msg)])
     q_nine = RadioField(questions[8]['9'], choices=L, validators=[InputRequired(message=msg)])
     q_ten = RadioField(questions[9]['10'], choices=L, validators=[InputRequired(message=msg)])
+    q_eleven = RadioField(questions[10]['11'], choices=L, validators=[InputRequired(message=msg)])
+    q_twelve = RadioField(questions[11]['12'], choices=L, validators=[InputRequired(message=msg)])
+    q_thirteen = RadioField(questions[12]['13'], choices=L, validators=[InputRequired(message=msg)])
+    q_fourteen = RadioField(questions[13]['14'], choices=L, validators=[InputRequired(message=msg)])
+    q_fifteen = RadioField(questions[14]['15'], choices=L, validators=[InputRequired(message=msg)])
+    q_sixteen = RadioField(questions[15]['16'], choices=L, validators=[InputRequired(message=msg)])
+    q_seventeen = RadioField(questions[16]['17'], choices=L, validators=[InputRequired(message=msg)])
+    q_eighteen = RadioField(questions[17]['18'], choices=L, validators=[InputRequired(message=msg)])
+    q_nineteen = RadioField(questions[18]['19'], choices=L, validators=[InputRequired(message=msg)])
+    q_twenty = RadioField(questions[19]['20'], choices=L, validators=[InputRequired(message=msg)])
     
 class ContactForm(FlaskForm):
     name = TextField('Name', validators=[InputRequired(message='Name required!')],              render_kw={"placeholder": "Enter name"})
@@ -224,6 +98,11 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        # return str(f.validate_user(form.user_email.data,form.user_pass.data))
+        if f.validate_user(form.user_email.data,form.user_pass.data) == 2:
+            return dashboard(f.get_user_first_name(form.user_email.data), form.user_email.data, m.get_possible_matches(form.user_email.data), l.get_mutual_likes(form.user_email.data))
+        # return form.user_email.data
     #if login information in database -> return 'dashboard'
     return render_template('login.html', form=form)
 
@@ -250,29 +129,63 @@ def signup():
     #return dashboard(L)
     
     if form.validate_on_submit() and Q.validate_on_submit():
-        #return form.first_name.data
-        return Q.q_one.data
+
+        L = []
+        L.append(form.first_name.data)
+        L.append(form.last_name.data)
+        L.append(form.user_email.data)
+        L.append(form.user_pass.data)
+        L.append(form.user_gender.data)
+        L.append(form.user_want_gender.data)
+        L.append(Q.q_one.data)
+        L.append(Q.q_two.data)
+        L.append(Q.q_three.data)
+        L.append(Q.q_four.data)
+        L.append(Q.q_five.data)
+        L.append(Q.q_six.data)
+        L.append(Q.q_seven.data)
+        L.append(Q.q_eight.data)
+        L.append(Q.q_nine.data)
+        L.append(Q.q_ten.data)
+        L.append(Q.q_eleven.data)
+        L.append(Q.q_twelve.data)
+        L.append(Q.q_thirteen.data)
+        L.append(Q.q_fourteen.data)
+        L.append(Q.q_fifteen.data)
+        L.append(Q.q_sixteen.data)
+        L.append(Q.q_seventeen.data)
+        L.append(Q.q_eighteen.data)
+        L.append(Q.q_nineteen.data)
+        L.append(Q.q_twenty.data)
+
+        if f.validate_registration(L) == 1:
+            f.validate_registration(L)
+            flash("Registration Success!")
+            return redirect(url_for('login'))
+        else:
+            flash("Email is already in use!")
+
     #if user not yet in database -> register
     
     #else -> print error message
     return render_template('signup.html', questions=questions, form=form, Q=Q)
 
-# @app.route('/signup', methods=['GET', 'POST'])
-# def signup():
-#     form = SignupForm(request.form)
-#     if request.method == 'POST' and form.validate():
-#         # user = User(form.username.data, form.email.data,
-#         #             form.password.data)
-#         # db_session.add(user)
-#         flash('Thanks for registering')
-#         return redirect(url_for('login'))
-#     return render_template('signup.html', form=form)
-
 @app.route('/dashboard')
-def dashboard(user_info, q_info = None):
+def dashboard(name = None, user_info = None, comp = None, mutual_likes = None):
     
-    return render_template('dashboard.html')
+    return render_template('dashboard.html',user_name = name, user=user_info, comp = comp, mutual_likes = mutual_likes)
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('index.html')
+
+@app.route('/like', methods=['POST'])
+def like():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
->>>>>>> denis
---
+++ Coffee and Bagels/match.py
@@ -0,0 +1,142 @@
+""" This is the prototype matching algorithm for the website
+
+V1.3
+"""
+
+import sqlite3 as sql
+
+#def of max/min compatability
+def min_max(person):
+    compat_range = [100.0,0.0]
+    try:
+        con = sql.connect("dating.db", timeout=10)
+        cur1 = con.cursor()
+        cur2 = con.cursor()
+
+        cur1.execute("select * from questionnaire where qemail = ?",(person,))
+        cur2.execute("select * from questionnaire")
+        for row1 in cur1:
+            for row2 in cur2:
+                if row1[0] == row2[0]:
+                    continue
+                temp = compatability(compare(row1[2:],row2[2:]))
+                if temp < compat_range[0]:
+                    compat_range[0] = temp
+                elif temp > compat_range[1]:
+                    compat_range[1] = temp
+
+        cur1.close()
+        cur2.close()
+        con.close()
+
+    except Exception as e:
+        raise e
+
+    print(compat_range)
+
+    return compat_range
+
+#definition for the comparison function
+def compare(p1,p2):
+    total_difference = 0
+    for x in range(len(p1)):
+        total_difference += abs(int(p1[x])-int(p2[x]))
+
+    return total_difference
+
+#definition for the compatability function
+def compatability(n):
+    if not n:
+        return 100
+    return ((100-n)/100)*100
+
+#adjusted compatability
+def adjusted_compatability(n,com_range):
+
+    try:
+        temp = float("{0:.2f}".format((((n-com_range[0])*100) / (com_range[1] - com_range[0]))))
+        return temp
+    except ZeroDivisionError as e:
+        raise e
+
+
+#main return function
+def get_matches(person):
+
+    qrange = min_max(person)
+    matches = []
+
+    try:
+        
+        con = sql.connect("dating.db", timeout=10)
+        cur1 = con.cursor()
+        cur2 = con.cursor()
+
+        p1_deets = cur1.execute("select * from users where uemail=?",(person,)).fetchone()
+        p1_ans = cur1.execute("select * from questionnaire where qemail=?",(person,)).fetchone()
+
+        person1 = (p1_deets,p1_ans)
+
+        cur1.execute("select * from users")
+        for row1 in cur1:
+            if person == row1[0]:
+                continue
+            else:
+                if person1[1][1] == row1[3]:
+                    cur2.execute("select * from questionnaire where qemail=?",(row1[0],))
+                    for row2 in cur2:
+                        if row2[1] == person1[0][3]:
+                            if adjusted_compatability(compatability(compare(person1[1][2:],row2[2:])),qrange) < 45.00:
+                                continue
+                            print(adjusted_compatability(compatability(compare(person1[1][2:],row2[2:])),qrange))
+                            matches.append([person,adjusted_compatability(compatability(compare(person1[1][2:],row2[2:])),qrange),row1[0]])
+                            # print("{0} {1} is %{2:.2f} compatible with {3} {4}".format(person1[0][1],person1[0][2],adjusted_compatability(compatability(compare(person1[1][2:],row2[2:])),qrange),row1[1],row1[2]))
+
+        cur1.close()
+        cur2.close()
+        con.close()
+        matches.sort(key=getkey, reverse=True)
+        return matches
+
+    except Exception as e:
+        raise e
+
+def getkey(item):
+    return item[1]
+
+
+
+def get_possible_matches(user_email):
+
+    con = sql.connect("dating.db", timeout=10)
+    cur1 = con.cursor()
+
+    try:
+
+        L = get_matches(user_email)
+        final = []
+        for row in L:
+            each_info = cur1.execute("select ufname, ulname, ugender from users where uemail = ?",(row[2],)).fetchone()
+
+            info_dict = {}
+
+            info_dict["First Name: "] = each_info[0]
+            info_dict["Last Name: "] = each_info[1]
+            info_dict["Gender: "] = each_info[2]
+            info_dict["Compatability: "] = "%"+str(row[1])
+            # info_dict["Email: "] = row[2]
+
+            final.append(info_dict)
+
+        return final
+
+    except Exception as e:
+
+        raise e
+
+
+if __name__ == "__main__":
+
+    get_matches('r.wells6894@gmail.com')
+    # for row in get_possible_matches('r.wells6894@gmail.com'):
+    #     print(row)
